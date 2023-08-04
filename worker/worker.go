e21bc5dc6757413e4948b64bf087b452dfe3dea3
monegim <mostafa.negim@gmail.com>
2023-08-04 10:41:46 +0330

    Add: worker methods

--- worker/worker.go
+++ worker/worker.go
@@ -1,9 +1,11 @@
 package worker
 
 import (
+	"cube/task"
+	"fmt"
+
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
-	"cube/task"
 )
 
 type Worker struct {
@@ -11,4 +13,19 @@ type Worker struct {
 	Queue     queue.Queue
 	Db        map[uuid.UUID]task.Task
 	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("I will collect stats")
+}
+
+func (w *Worker) RunTask() {
+	fmt.Println("I will start or stop a task")
+}
+
+func (w *Worker) StartTask() {
+	fmt.Println("I will start a task")
+}
+func (w *Worker) StopTask() {
+	fmt.Println("I will stop a task")
 }
