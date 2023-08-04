c9e3d413d0a2006b59efd9dae0ec5013479b1c8b
monegim <mostafa.negim@gmail.com>
2023-08-04 10:41:46 +0330

    Add: state_machine.go

---
+++ task/state_machine.go
@@ -0,0 +1,25 @@
+package task
+
+var stateTransitionMap = map[State][]State{
+	Pending:   []State{Scheduled},
+	Scheduled: []State{Scheduled, Running, Failed},
+	Running:   []State{Running, Completed, Failed},
+	Completed: []State{},
+	Failed:    []State{},
+}
+
+func Contains(states []State, state State) bool {
+	for _, s := range states {
+		if s == state {
+			return true
+		}
+	}
+	return false
+}
+
+func ValidStateTransition(src State, dst State) bool {
+	if Contains(stateTransitionMap[src], dst) {
+		return true
+	}
+	return false
+}
