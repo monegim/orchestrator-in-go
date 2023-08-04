c3d35858f68bbca51fe0184b039cad6b59cd7276
monegim <mostafa.negim@gmail.com>
2023-08-04 11:20:25 +0330

    Add: SelectWorker

--- manager/manger.go
+++ manager/manger.go
@@ -15,10 +15,19 @@ type Manager struct {
 	Workers       []string
 	WorkerTaskMap map[string][]uuid.UUID
 	TaskWorkerMap map[uuid.UUID]string
+	LastWorker    int
 }
 
-func (m *Manager) SelectWorker() {
-	fmt.Println("I will select an appropriate worker")
+func (m *Manager) SelectWorker() string {
+	var newWorker int
+	if m.LastWorker+1 < len(m.Workers) {
+		newWorker = m.LastWorker + 1
+		m.LastWorker++
+	} else {
+		newWorker = 0
+		m.LastWorker = 0
+	}
+	return m.Workers[newWorker]
 }
 func (m *Manager) UpdateTasks() {
 	fmt.Println("I will update tasks")
