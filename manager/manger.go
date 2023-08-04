caf548ce998b8414146fbdf9499654aa0424d04d
monegim <mostafa.negim@gmail.com>
2023-08-04 12:12:34 +0330

    Add SendWork

--- manager/manger.go
+++ manager/manger.go
@@ -1,8 +1,13 @@
 package manager
 
 import (
+	"bytes"
 	"cube/task"
+	"cube/worker"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
@@ -10,8 +15,8 @@ import (
 
 type Manager struct {
 	Pending       queue.Queue
-	TaskDb        map[string][]task.Task
-	EventDb       map[string][]task.TaskEvent
+	TaskDb        map[uuid.UUID]*task.Task
+	EventDb       map[uuid.UUID]*task.TaskEvent
 	Workers       []string
 	WorkerTaskMap map[string][]uuid.UUID
 	TaskWorkerMap map[uuid.UUID]string
@@ -33,5 +38,51 @@ func (m *Manager) UpdateTasks() {
 	fmt.Println("I will update tasks")
 }
 func (m *Manager) SendWork() {
-	fmt.Println("I will send work to workers")
+	if m.Pending.Len() > 0 {
+		w := m.SelectWorker()
+
+		e := m.Pending.Dequeue()
+		te := e.(task.TaskEvent)
+		t := te.Task
+		log.Printf("Pulled %v off pending queue\n", t)
+
+		m.EventDb[te.ID] = &te
+		m.WorkerTaskMap[w] = append(m.WorkerTaskMap[w], t.ID)
+		m.TaskWorkerMap[t.ID] = w
+
+		t.State = task.Scheduled
+		m.TaskDb[t.ID] = &t
+
+		data, err := json.Marshal(te)
+		if err != nil {
+			log.Printf("Unable to marshal task object: %v.", t)
+		}
+		url := fmt.Sprintf("http://%s/tasks", w)
+		resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+		if err != nil {
+			log.Printf("Error connecting to %v: %v", w, err)
+			m.Pending.Enqueue(t)
+			return
+		}
+		d := json.NewDecoder(resp.Body)
+		if resp.StatusCode != http.StatusCreated {
+			e := worker.ErrResponse{}
+			err := d.Decode(&d)
+			if err != nil {
+				fmt.Printf("Error decoding response: %s\n", err.Error())
+				return
+			}
+			log.Printf("Response error (%d): %s", e.HTTPStatusCode, e.Message)
+			return
+		}
+		t = task.Task{}
+		err = d.Decode(&t)
+		if err != nil {
+			fmt.Printf("Error decoding response: %s\n", err.Error())
+			return
+		}
+		log.Printf("%#v\n", t)
+	} else {
+		log.Println("No work in the queue")
+	}
 }
