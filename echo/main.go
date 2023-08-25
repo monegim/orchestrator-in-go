b0bebd8ccbde85d5e64ee4c1553d9959129b8c8d
monegim <mostafa.negim@gmail.com>
2023-08-25 11:03:43 +0330

    Add: echo

--- echo/main.go
+++ echo/main.go
@@ -1,5 +1,34 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-chi/chi/v5"
+)
+
 type Message struct {
 	Msg string
+}
+
+const addr = "0.0.0.0:7777"
+
+func main() {
+	r := chi.NewRouter()
+	srv := &http.Server{
+		Addr:    addr,
+		Handler: r,
+	}
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGKILL, syscall.SIGTERM)
+	<- c
+
 }
